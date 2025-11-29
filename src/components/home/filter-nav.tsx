f490309adbee869b7ffc05aaedbbbfa179fcf9b0
feFCProject <suteja.indrayana@flyingcape.com.sg>
2025-11-29 21:41:08 +0700

    fixed filter nav layout

--- src/components/home/filter-nav.tsx
+++ src/components/home/filter-nav.tsx
@@ -68,8 +68,21 @@ export const FilterNav = () => {
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-between gap-10 md:gap-4">
-      <NavigationMenu className="filter-nav bg-c-gray font-anton-sc corner-shape-bevel text-white/50 max-w-sm md:max-w-full overflow-x-auto scrollbar-hide flex-1">
-        <NavigationMenuList className="flex px-5">
+      <NavigationMenu
+        className="
+        filter-nav 
+        bg-c-gray 
+        font-anton-sc 
+        corner-shape-bevel 
+        text-white/50 
+        max-w-sm md:max-w-full 
+        overflow-x-auto 
+        whitespace-nowrap 
+        scrollbar-hide 
+        flex-1
+    "
+      >
+        <NavigationMenuList className="flex gap-4 md:gap-0 px-0">
           {navbar.map((item, index) => (
             <NavigationMenuItem key={index}>
               <NavigationMenuLink
@@ -83,37 +96,39 @@ export const FilterNav = () => {
         </NavigationMenuList>
       </NavigationMenu>
       <div className="flex">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              size={"lg"}
-              className="button-bg font-anton-sc m-auto cursor-pointer border-0 target:border-0 flex items-center gap-"
-            >
-              Filters <ListFilter />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="bg-c-gray mx-10 rounded-none border-none p-5 text-white md:w-md">
-            <DropdownMenuCheckboxItem
-              checked={showStatusBar}
-              onCheckedChange={setShowStatusBar}
-            >
-              Newest
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem
-              checked={showActivityBar}
-              onCheckedChange={setShowActivityBar}
-              disabled
-            >
-              Oldest
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem
-              checked={showPanel}
-              onCheckedChange={setShowPanel}
-            >
-              Popular
-            </DropdownMenuCheckboxItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex-1">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                size={"lg"}
+                className="button-bg font-anton-sc m-auto cursor-pointer border-0 target:border-0 flex items-center gap-"
+              >
+                Filters <ListFilter />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-c-gray mx-10 rounded-none border-none p-5 text-white md:w-md">
+              <DropdownMenuCheckboxItem
+                checked={showStatusBar}
+                onCheckedChange={setShowStatusBar}
+              >
+                Newest
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={showActivityBar}
+                onCheckedChange={setShowActivityBar}
+                disabled
+              >
+                Oldest
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={showPanel}
+                onCheckedChange={setShowPanel}
+              >
+                Popular
+              </DropdownMenuCheckboxItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
         <Button
           size={"lg"}
           className="button-search-bg font-anton-sc cursor-pointer border-0 target:border-0"
