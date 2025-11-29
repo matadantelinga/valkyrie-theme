87d6d54a5bc29dd846c02409a239611bc36f7df5
feFCProject <suteja.indrayana@flyingcape.com.sg>
2025-11-29 21:11:09 +0700

    fixed main banner

--- src/components/layout/banner-slider.tsx
+++ src/components/layout/banner-slider.tsx
@@ -3,17 +3,45 @@ import Image from "next/image";
 
 export default function BannerSlider() {
   return (
-    <div className="bg-c-red relative mb-20 flex flex-col-reverse items-center justify-center md:mb-0 md:flex-row md:justify-between">
-      <div className="w-[5%]">
-        <div className="dots py-4 flex items-center flex-col justify-center">
-          <ul className="flex flex-col gap-2">
-            <li className="dot"></li>
-            <li className="dot active"></li>
-            <li className="dot"></li>
-          </ul>
+    <section className="main-banner-wrapper relative">
+      <div className="bg-c-red relative mb-20 flex flex-wrap items-center justify-center md:mb-0 md:flex-row md:justify-between">
+        <div className="w-[5%] dots-wrapper">
+          <div className="dots py-4 flex items-center flex-col justify-center">
+            <ul className="flex flex-col gap-2">
+              <li className="dot"></li>
+              <li className="dot active"></li>
+              <li className="dot"></li>
+            </ul>
+          </div>
+        </div>
+        {/* Desktop Title*/}
+        <div className="w-1/3 px-8 text-center hidden md:static md:block md:text-start">
+          <h1 className="font-anton-sc text-4xl font-bold tracking-wider text-white md:text-6xl">
+            BIG TITLE
+          </h1>
+          <p className="hero-subtitle mt-4 max-w-md text-sm text-white opacity-50">
+            Landing a Loki Enchantment symbol will turn the symbol into
+          </p>
+          <Button
+            className="font-anton-sc text-c-red mt-8 flex w-full cursor-pointer justify-between rounded-none bg-white text-xl hover:text-white md:w-auto md:justify-start md:space-x-3"
+            size={"lg"}
+          >
+            <span>READ MORE</span>
+            <Image
+              src={"/icons/arrow.svg"}
+              alt={"viking"}
+              width={12}
+              height={12}
+            />
+          </Button>
+        </div>
+        {/* End Desktop Title*/}
+        <div className="flex-1">
+          <div className="banner-image"></div>
         </div>
       </div>
-      <div className="w-1/3 px-8 text-center md:static md:block md:text-start">
+      {/* Mobile Title*/}
+      <div className="mobile-slide-title w-full px-8 text-center block md:hidden">
         <h1 className="font-anton-sc text-4xl font-bold tracking-wider text-white md:text-6xl">
           BIG TITLE
         </h1>
@@ -33,17 +61,6 @@ export default function BannerSlider() {
           />
         </Button>
       </div>
-      <div className="flex-1">
-        <div className="banner-image">
-          {/* <Image
-            src={"/images/viking.png"}
-            alt={"viking"}
-            width={1000}
-            height={1000}
-            className="w-full md:h-full"
-          /> */}
-        </div>
-      </div>
-    </div>
+    </section>
   );
 }
